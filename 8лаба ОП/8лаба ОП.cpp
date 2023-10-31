6379a8dbe7c32b2e59b5c394af5f1aeaa4522a59
BotnightTown <minecraftv871@gmail.com>
2023-10-31 17:08:13 +0200

    Add project files.

---
+++ 8лаба ОП/8лаба ОП.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <iomanip>
+#include "windows.h"
+#include <cmath>
+
+using namespace std;
+
+float Sum(int, int);
+
+float Sum(int a, int b)
+{
+	int sum = 0;
+	sum = sum + sqrt(a + b);
+	float y;
+	y = sum;
+	return y;
+}
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	int a, b;
+	cout << "Введіть a = ";
+	cin >> a;
+	cout << "Введіть b = ";
+	cin >> b;
+	float rez;
+	rez = Sum(a, b);
+	cout << setw(10) << setprecision(3) << "Y= " << rez << endl;
+
+}
